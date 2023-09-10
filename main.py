ba7929fd5b81abd5fde81d37a04454cb484ea03a
MaxKomar15 <143327983+MaxKomar15@users.noreply.github.com>
2023-09-10 15:31:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import pygame
-print("Hello")
+class Title:
+    def __init__(self, title_text, x, y):
+        self.x = x
+        self.y = y
+        self.appear = True
+
+    def print_info(self):
+        print("Кнопка", self.title)
+        print("Розташування: (", self.x, self.y, ")")
+        print("Видимість:", self.appear)
+
+    def hide(self):
+        self.apear = False
+        print("Напис приховано")
+
+t1 = Title("Ок", 200, 250)
+t1.print_info()
+t1.hide()
